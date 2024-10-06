3737f12250732a3c25dc7cc08cfc1d7ee8db3bf6
inthoking <emisaelvean@gmail.com>
2024-10-06 00:25:48 -0400

    unique en user

--- app/models/user.py
+++ app/models/user.py
@@ -5,10 +5,10 @@ from app.database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column("User_id", Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
-    nickname = Column("User_name", String(50), nullable=False)
+    nickname = Column("User_name", String(50), nullable=False, unique=True)
     name = Column("Name", String(50), nullable=False)
     lastName = Column("Last_name", String(50), nullable=False)
-    email = Column("Email_add", String(100), nullable=False)
+    email = Column("Email_add", String(100), nullable=False, unique=True)
     masters = relationship("Master", back_populates="user")
     passwords = relationship("Pass", back_populates="user")
 
